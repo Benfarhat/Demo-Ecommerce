0a1a38fd94c65c25875cc92756b06fc24f02a540
Benfarhat <benfarhat.elyes@gmail.com>
2018-11-02 00:07:12 +0100

    Enable JSONDoc

--- src/main/java/demo/benfarhat/ecommerce/EcommerceApplication.java
+++ src/main/java/demo/benfarhat/ecommerce/EcommerceApplication.java
@@ -1,9 +1,11 @@
 package demo.benfarhat.ecommerce;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJSONDoc
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
